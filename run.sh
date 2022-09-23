b585b6189ea4c56ba1963936b417bf95a55c60e5
Joe Roberts <joe.t.roberts@jpl.nasa.gov>
2022-09-22 18:53:09 -0700

    Updated vizgen version and run script

--- run.sh
+++ run.sh
@@ -6,9 +6,12 @@ source ./version.sh
 docker run -it \
     --restart unless-stopped \
     --name coverage-vizgen \
-    -v $(pwd)/vizgen/:/vizgen/ \
 	-v $(pwd)/colormaps/:/vizgen/colormaps/ \
+	-v $(pwd)/configs/:/vizgen/configs/ \
 	-v $(pwd)/data/:/vizgen/data/ \
+	-v $(pwd)/empty_tiles/:/vizgen/empty_tiles/ \
     -v $(pwd)/output/:/vizgen/output/ \
     -v $(pwd)/working/:/vizgen/working/ \
     coverage/vizgen:$VIZGEN_VERSION /bin/bash
+
+    
